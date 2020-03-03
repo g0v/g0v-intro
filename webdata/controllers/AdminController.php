fe6bc7068cbfda7970a289859792d232c2740f8b
Ronny Wang <ronnywang@gmail.com>
2020-03-03 16:55:10 +0800

    add event modify/add feature

--- webdata/controllers/AdminController.php
+++ webdata/controllers/AdminController.php
@@ -16,5 +16,32 @@ class AdminController extends Pix_Controller
 
     public function eventAction()
     {
+        if (array_key_exists('event_id', $_GET) and $event = Event::find(strval($_GET['event_id']))) {
+            $this->view->event = $event;
+        }
+    }
+
+    public function editeventAction()
+    {
+        if ($_POST['sToken'] != Session::getStoken()) {
+            return $this->alert('sToken error', '/admin/event');
+        }
+        if (array_key_exists('event_id', $_GET)) {
+            if (!$event = Event::find(strval($_GET['event_id']))) {
+                return $this->alert("event {$_GET['event_id']} not found", '/admin/event');
+            }
+            $event->update(array(
+                'id' => $_POST['id'],
+                'name' => $_POST['name'],
+                'status' => intval($_POST['status']),
+            ));
+        } else {
+            $event = Event::insert(array(
+                'id' => $_POST['id'],
+                'name' => $_POST['name'],
+                'status' => intval($_POST['status']),
+            ));
+        }
+        return $this->alert('ok', '/admin/event?event_id' . urlencode($event->id));
     }
 }
