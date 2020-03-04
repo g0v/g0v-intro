8068c1d537f725ea08a27117defb012ee7a5e651
Ronny Wang <ronnywang@gmail.com>
2020-03-04 13:52:05 +0800

    add basic meet feature

--- webdata/controllers/MeetController.php
+++ webdata/controllers/MeetController.php
@@ -17,4 +17,34 @@ class MeetController extends Pix_Controller
         }
         $this->view->event = $event;
     }
+
+    public function channelAction()
+    {
+        list(, /*meet*/, /*channel*/, $event_id, $channel_id) = explode('/', $this->getURI());
+        if (!$event = Event::find($event_id)) {
+            return $this->alert("event not found {$event_id}", '/');
+        }
+        if (!$channel = Channel::find(intval($channel_id)) or $channel->event_id != $event_id) {
+            return $this->alert("channel_id not found {$channel_id}", '/');
+        }
+        $this->view->event = $event;
+        $this->view->channel = $channel;
+    }
+
+    public function dataAction()
+    {
+        list(, /*meet*/, /*channel*/, $event_id, $channel_id) = explode('/', $this->getURI());
+        if (!$event = Event::find($event_id)) {
+            return $this->alert("event not found {$event_id}", '/');
+        }
+        if (!$channel = Channel::find(intval($channel_id)) or $channel->event_id != $event_id) {
+            return $this->alert("channel_id not found {$channel_id}", '/');
+        }
+        $ret = new StdClass;
+        $ret->data = $channel->getData();
+        $ret->meta = $channel->getStatus()->getMeta();
+        $ret->user = new StdClass;
+        $ret->user->slack_id = $this->view->user->slack_id;
+        return $this->json($ret);
+    }
 }
