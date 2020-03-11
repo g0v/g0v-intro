3d3500044c3a8396b8bdccf189701ab40840457f
Ronny Wang <ronnywang@gmail.com>
2020-03-11 17:11:06 +0800

    add /message/getmessage API to get message

--- webdata/controllers/MessageController.php
+++ webdata/controllers/MessageController.php
@@ -35,4 +35,29 @@ class MessageController extends Pix_Controller
 
         return $this->json(1);
     }
+
+    public function getmessageAction()
+    {
+        $channel_id = $_GET['channel'];
+        if (!$channel_id) {
+            return $this->json(0);
+        }
+
+        $after = floatval($_GET['after']);
+        $ret = new StdClass;
+        $ret->messages = array();
+        if (!$after) {
+            $messages = MessageLog::search(array('channel' => $channel_id))->order('ts DESC')->limit(3);
+        } else {
+            $messages = MessageLog::search(array('channel' => $channel_id))->order('ts DESC')->search('ts > ' . $after)->limit(3);
+        }
+        foreach ($messages as $message) {
+            $data = json_decode($message->data);
+            $data->user = User::find($data->user)->account;
+            $ret->messages[] = $data;
+            $after = max($after, $message->ts);
+        }
+        $ret->next_url = '/message/getmessage?channel=' . urlencode($channel_id) . '&after=' . $after;
+        return $this->json($ret);
+    }
 }
