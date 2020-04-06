129a1104fbf6a7981329ead620417570d0ad8147
Ronny Wang <ronnywang@gmail.com>
2020-04-06 21:11:30 +0800

    add seq column on Event table

--- webdata/models/Event.php
+++ webdata/models/Event.php
@@ -30,14 +30,17 @@ class Event extends Pix_Table
     public function init()
     {
         $this->_name = 'event';
-        $this->_primary = 'id';
+        $this->_primary = 'seq';
         $this->_rowClass = 'EventRow';
 
+        $this->_columns['seq'] = array('type' => 'int', 'auto_increment' => true);
         $this->_columns['id'] = array('type' => 'varchar', 'size' => 16);
         $this->_columns['name'] = array('type' => 'text');
         $this->_columns['created_at'] = array('type' => 'int');
         // 0 - 徵集中, 1 - 已結束
         $this->_columns['status'] = array('type' => 'int');
         $this->_columns['data'] = array('type' => 'text');
+
+        $this->addIndex('id', array('id'), 'unique');
     }
 }
