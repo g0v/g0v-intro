b104d078e939931531f0074846cb0b4820bc3b21
Ronny Wang <ronnywang@gmail.com>
2022-03-12 17:44:06 +0800

    add bot log

--- webdata/controllers/BotController.php
+++ webdata/controllers/BotController.php
@@ -22,17 +22,38 @@ class BotController extends Pix_Controller
         if ($_POST['sToken'] != Session::getStoken()) {
             return $this->alert('stoken error', '/bot');
         }
-        $bot = BotToken::insert([
-            'owner' => $this->view->user->slack_id,
-            'created_at' => time(),
-            'token' => Helper::uniqid(32),
-            'data' => json_encode([
-                'name' => strval($_POST['name']),
-                'purpose' => strval($_POST['purpose']),
-                'channels' => strval($_POST['channels']),
-                'displayname' => strval($_POST['displayname']),
-            ]),
-        ]);
+
+        if (array_key_exists('id', $_GET)) {
+            if (!$bot = BotToken::find(intval($_GET['id'])) or $bot->owner != $this->view->user->slack_id) {
+                return $this->alert('bot not found', '/bot');
+            }
+            $bot->update([
+                'data' => json_encode([
+                    'name' => strval($_POST['name']),
+                    'purpose' => strval($_POST['purpose']),
+                    'channels' => strval($_POST['channels']),
+                    'displayname' => strval($_POST['displayname']),
+                ]),
+            ]);
+            $text = sprintf("%s(%s) 更新機器人資料，機器人名稱=%s, 機器人目的=%s, 機器人發言頻道=%s, 機器人顯示名稱=%s", $this->view->user->getDisplayName(), $this->view->user->slack_id, $_POST['name'], $_POST['purpose'], $_POST['channels'], $_POST['displayname']);
+        } else {
+            $bot = BotToken::insert([
+                'owner' => $this->view->user->slack_id,
+                'created_at' => time(),
+                'token' => Helper::uniqid(32),
+                'data' => json_encode([
+                    'name' => strval($_POST['name']),
+                    'purpose' => strval($_POST['purpose']),
+                    'channels' => strval($_POST['channels']),
+                    'displayname' => strval($_POST['displayname']),
+                ]),
+            ]);
+            $text = sprintf("%s(%s) 新增機器人，機器人名稱=%s, 機器人目的=%s, 機器人發言頻道=%s, 機器人顯示名稱=%s", $this->view->user->getDisplayName(), $this->view->user->slack_id, $_POST['name'], $_POST['purpose'], $_POST['channels'], $_POST['displayname']);
+        }
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, sprintf('https://slack.com/api/chat.postMessage?token=%s&channel=%s&username=%s', urlencode(getenv('SLACK_ACCESS_TOKEN')), urlencode('#jothonbot-log'), urlencode('揪松機器人記錄')));
+        curl_setopt($curl, CURLOPT_POSTFIELDS, 'text=' . urlencode($text));
+        curl_exec($curl);
         return $this->redirect('/bot/?id=' . $bot->token_id);
     }
 }
